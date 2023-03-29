28771e98b4bad338b15e70385859a5f78334a237
Ishihara <almond1024r@gmail.com>
2023-03-30 00:32:46 +0900

    c

--- src/routes/TopPage/Component/Tabs/FourthTab.jsx
+++ src/routes/TopPage/Component/Tabs/FourthTab.jsx
@@ -8,6 +8,7 @@ export const FourthTab=()=>{
       const response=await axios.get("https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426?format=json&keyword=%E3%82%B2%E3%83%AC%E3%83%B3%E3%83%87%20%E3%82%B9%E3%82%AD%E3%83%BC%20%E3%82%B9%E3%83%8E%E3%83%9C&hits=30&applicationId=1001318956766170232");
       const result=response.data;
       const resultStr=JSON.stringify(result);
+      console.log(resultStr);
       const obj=JSON.parse(resultStr);
       const getones=JSON.stringify(obj.hotels);
       const jsonAPIs=JSON.parse(getones);
@@ -24,11 +25,15 @@ export const FourthTab=()=>{
           return(
             <>
              <div key={index} className ="fourth-tab-contents">
-               <div style={{backgroundImage:`url(${api.hotel[0].hotelBasicInfo.hotelImageUrl})`}} className="fourth-tab-top"></div>
+               {/* <div style={{backgroundImage:`url(${api.hotel[0].hotelBasicInfo.hotelImageUrl})`}} className="fourth-tab-top"></div> */}
+                <div className="fourth-tab-top">
+                  <a href={api.hotel[0].hotelBasicInfo.hotelInformationUrl}>
+                   <img src={api.hotel[0].hotelBasicInfo.hotelImageUrl} alt="#"></img>
+                  </a>
+                </div>
                 <div className="fourth-tab-bottom"> 
                   <h4>{api.hotel[0].hotelBasicInfo.hotelName}</h4>
                   <p>{api.hotel[0].hotelBasicInfo.hotelSpecial}</p>
-                  <a href={api.hotel[0].hotelBasicInfo.hotelInformationUrl}>リンク先</a>
                 </div>
               </div>
             </>)
