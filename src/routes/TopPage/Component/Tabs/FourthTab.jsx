456a9f21eb2622aa305ee5613056b7eeefccfbdd
Ishihara <almond1024r@gmail.com>
2023-03-30 00:46:16 +0900

    css調整

--- src/routes/TopPage/Component/Tabs/FourthTab.jsx
+++ src/routes/TopPage/Component/Tabs/FourthTab.jsx
@@ -3,6 +3,32 @@ import axios from "axios";
 
 export const FourthTab=()=>{
     const [apiHotel, setHotel] = useState([]);
+    const tab = {
+      width: '100%',
+      height:'100%'
+    };
+    const tabContainer={
+      display:'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center'
+    }
+    const tabContents={
+      width: '31%',
+      height: '370px',
+      margin:'20px 15px',
+      backgroundColor: 'white',
+      boxShadow: '0 0 4px',
+      paddingBottom:'50px',
+      color:'#444444'
+    }
+    const tabTop={
+      width: '100%',
+      height: '60%'
+    }
+    const tabBottom={
+      padding:'0px 10px',
+      border: 'none'
+    }
     useEffect(()=>{
     const getAPI=async()=>{
       const response=await axios.get("https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426?format=json&keyword=%E3%82%B2%E3%83%AC%E3%83%B3%E3%83%87%20%E3%82%B9%E3%82%AD%E3%83%BC%20%E3%82%B9%E3%83%8E%E3%83%9C&hits=30&applicationId=1001318956766170232");
@@ -18,20 +44,20 @@ export const FourthTab=()=>{
     },[]);
     return(
       <>
-      <div  className="fourth-tab">
-      <div className="fourth-tab-container">
+      <div style={tab}>
+      <div style={tabContainer}>
          {apiHotel.map((api,index)=>{
           console.log(api.hotel[0].hotelBasicInfo.hotelImageUrl);
           return(
             <>
-             <div key={index} className ="fourth-tab-contents">
+             <div key={index} style={tabContents}>
                {/* <div style={{backgroundImage:`url(${api.hotel[0].hotelBasicInfo.hotelImageUrl})`}} className="fourth-tab-top"></div> */}
-                <div className="fourth-tab-top">
+                <div style={tabTop}className="fourth-tab-top">
                   <a href={api.hotel[0].hotelBasicInfo.hotelInformationUrl}>
                    <img src={api.hotel[0].hotelBasicInfo.hotelImageUrl} alt="#"></img>
                   </a>
                 </div>
-                <div className="fourth-tab-bottom"> 
+                <div style={tabBottom}> 
                   <h4>{api.hotel[0].hotelBasicInfo.hotelName}</h4>
                   <p>{api.hotel[0].hotelBasicInfo.hotelSpecial}</p>
                 </div>
