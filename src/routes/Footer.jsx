9b00aeab0f8421c99cb07bfd11263b59b68afc00
Ishihara <almond1024r@gmail.com>
2023-04-01 11:07:29 +0900

    ss

--- src/routes/Footer.jsx
+++ src/routes/Footer.jsx
@@ -1,28 +1,41 @@
 
-// const Footer=()=>{
-//     return(
-//         <div className="footer">
-//         <ul className="menu">
-//           <li>
-//             <a href="https://github.com/D1nast">Github</a>
-//           </li>
-//           <li>
-//             <a href="#">Todoアプリ</a>
-//           </li>
-//           <li>
-//             <a href="#">CCCCC</a>
-//           </li>
-//           <li>
-//             <a href="#">DDDDD</a>
-//           </li>
-//           <li>
-//             <a href="#">EEEEE</a>
-//           </li>
-//         </ul>
-//         <p className="footerMessage">D1nast</p>
-//     </div>
+const Footer=()=>{
+    const footer={
+        main:{
+            width: '100%',
+            height: '72px',
+            color: '#efefef',
+            backgroundColor: '#3d3d3d',
+            padding: '15px 0',
+        },
+        a:{
+            textDecoration: 'none',
+            color: '#efefef',
+        }
+    }
+    return(
+        <div className="footer">
+        <ul className="menu">
+          <li>
+            <a href="https://github.com/D1nast">Github</a>
+          </li>
+          <li>
+            <a href="#">Todoアプリ</a>
+          </li>
+          <li>
+            <a href="#">CCCCC</a>
+          </li>
+          <li>
+            <a href="#">DDDDD</a>
+          </li>
+          <li>
+            <a href="#">EEEEE</a>
+          </li>
+        </ul>
+        <p className="footerMessage">D1nast</p>
+    </div>
     
-//     );
-// }
+    );
+}
 
-// export default Footer;
+export default Footer;
