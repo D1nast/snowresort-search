f4f03cb610694e05494ab2db6434925b113d11ec
Ishihara <almond1024r@gmail.com>
2023-04-22 19:24:15 +0900

    as

--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import {React} from "react";
 
 export const Header=()=>{
@@ -16,28 +16,30 @@ export const Header=()=>{
         margin: '0',
         lineHeight: '60px',
         float: 'left',
+        fontSize:'25px',
+        paddingLeft:'25px',
     }
-    const nav={
-        main:{
-            listStyle: 'none',
-            float: 'right',
-        },
-        li:{
-            display: 'inline-block',
-        },
-        a:{
-            display: 'inline-block',
-            padding: '3px 15px',
-            color: 'inherit',
-            textDecoration: 'none',
-        }
-    }
+    // const nav={
+    //     main:{
+    //         listStyle: 'none',
+    //         float: 'right',
+    //     },
+    //     li:{
+    //         display: 'inline-block',
+    //     },
+    //     a:{
+    //         display: 'inline-block',
+    //         padding: '3px 15px',
+    //         color: 'inherit',
+    //         textDecoration: 'none',
+    //     }
+    // }
 
     return(
     <div style={header}>
      <div className="container">
-        <h1 style={logo}>Top</h1>
-        <ul style={nav.main}>
+        <h1 style={logo}>ゲレンデ・宿検索</h1>
+        {/* <ul style={nav.main}>
         <li style={nav.li}>
             <Link to="/" style={nav.a}>HOME</Link>
         </li>
@@ -50,7 +52,7 @@ export const Header=()=>{
         <li style={nav.li}>
              <Link to="about" style={nav.a}>About</Link>
         </li>
-        </ul>
+        </ul> */}
      </div>
     </div>
     );
